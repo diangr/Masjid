457951bd0f68f36d5408229ee1cd945b448351a9
diangr <diangr2921@gmail.com>
2018-03-20 06:26:34 +0700

    update

---
+++ dist/js/style.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.animated-icon1,.animated-icon3,.animated-icon4').click(function(){
+        $(this).toggleClass('open');
+    });
+});
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+})
